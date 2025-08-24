00aace9dbab8fd22319c1977bed510749fa32fc4
Vaibhav_gupta <vaibhav_gupta@Vaibhavs-MacBook-Air.local>
2025-08-24 09:40:31 +0530

    Added unit tests for Two Sum problem in Python

---
+++ tests/test_two_sum.py
@@ -0,0 +1,11 @@
+import unittest
+from Python.Arrays.two_sum import twoSum
+
+class TestTwoSum(unittest.TestCase):
+    def test_examples(self):
+        self.assertEqual(twoSum([2,7,11,15], 9), [0,1])
+        self.assertEqual(twoSum([3,2,4], 6), [1,2])
+        self.assertEqual(twoSum([3,3], 6), [0,1])
+
+if __name__ == "__main__":
+    unittest.main()
